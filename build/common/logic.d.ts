a24969498f34d506a66ac7b9b712afe84fedf50f
TWscripter <vaclav.stengl@creativedock.com>
2021-04-30 21:00:03 +0200

    rm build from git

--- build/common/logic.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Create number formatter for game locale which will use default value when formatted values is null
- */
-declare function numberFormatterClosure(defaultValue: string): (value?: number) => string;
-declare function parseIntAndRemoveNonNum(input: string): number;
-export { numberFormatterClosure, parseIntAndRemoveNonNum };
